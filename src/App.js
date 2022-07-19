399f36cc69316266858d1420ac687fbe6927914b
Martensit2019 <spichakandrey79@yandex.ru>
2022-07-19 16:37:54 +0300

    add pagination

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import Users from './components/users'
-import SearchStatus from './components/searchStatus'
-import api from './api'
+import Users from './app/components/users'
+import SearchStatus from './app/components/searchStatus'
+import api from './app/api'
 
 const App = () => {
   const [users, setUsers] = useState(api.users.fetchAll())
---
+++ src/app/utils/paginate.js
@@ -0,0 +1,4 @@
+export function paginate (items, pageNumber, pageSize){
+  const startIdx = (pageNumber - 1) * pageSize
+  return [...items].splice(startIdx, pageSize)
+}
