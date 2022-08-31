2fbf1c1d233804aaaf8af095be12f000588f2a7e
Martensit2019 <spichakandrey79@yandex.ru>
2022-08-31 14:36:52 +0300

    fix sex

--- src/app/api/fake.api/professions.api.js
+++ src/app/api/fake.api/professions.api.js
@@ -17,8 +17,8 @@ export const professions = [
 const fetchAll = () =>
   new Promise((resolve) => {
     window.setTimeout(function () {
-      resolve(professions);
-      // resolve(professionsObject);
+      // resolve(professions);
+      resolve(professionsObject);
     }, 2000);
   });
 
